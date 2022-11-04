9c0e119bf8abc26005daa3d7b3d7b0c2c77acf77
joaocasr <castrojoao839@gmail.com>
2022-11-04 00:19:33 +0000

    construcao do grafo

--- Grafo.py
+++ Grafo.py
@@ -1,5 +1,69 @@
 class Grafo:
-    def __init__(self, oriented = False):
+    def __init__(self, nodos,oriented = False):
         self.oriented = oriented
-        self.nodos = []
+        self.nodos = nodos
+        self.visitados = []
         self.grafo = {}
+    
+    
+    def devolveNome(self,x,y):
+        for peca in self.nodos:
+            if(peca.get_x()==x and peca.get_y()==y):
+                 return peca.get_nome()
+        return None
+
+    def devolvePeca(self,x,y):
+        for peca in self.nodos:
+            if(peca.get_x()==x and peca.get_y()==y):
+                 return peca
+        return None
+    
+    def devolvePecabyNome(self,node):
+        for peca in self.nodos:
+            if(peca.get_nome().__eq__(node.get_nome())):
+                 return peca
+        return None
+
+    def existePeca(self,x,y):
+        for peca in self.nodos:
+            if(peca.get_x()==x and peca.get_y()==y):
+                 return True
+        return False
+
+    #devolve lista de nodos adjacentes
+    def devolveAdjs(self,x,y):
+        peca = self.devolvePeca(x,y)
+        x = peca.get_x()
+        y = peca.get_y()
+        adjacentes = list()
+        if(self.existePeca(x+1,y)): adjacentes.append(self.devolvePeca(x+1,y))
+        if(self.existePeca(x-1,y)): adjacentes.append(self.devolvePeca(x-1,y))
+        if(self.existePeca(x,y+1)): adjacentes.append(self.devolvePeca(x,y+1))
+        if(self.existePeca(x,y-1)): adjacentes.append(self.devolvePeca(x,y-1))
+        for p in adjacentes:
+            if(p.get_nome() in self.visitados):
+                    adjacentes.remove(p)
+        return adjacentes
+
+    def constroiGrafo(self,start):
+        adjs = self.devolveAdjs(start.get_x(),start.get_y())
+        for a in adjs:
+            self.adicionaAresta(start,a,1)
+        self.visitados.append(start.get_nome())
+        
+
+    def adicionaAresta(self, node1, node2, weight):
+        if (node1.get_nome() not in self.grafo.keys()):
+            self.grafo[node1.get_nome()] = list()
+        if (node2.get_nome() not in self.grafo.keys()):
+            self.grafo[node2.get_nome()] = list()
+        if((node2.get_nome(),weight) not in self.grafo[node1.get_nome()]):
+            self.grafo[node1.get_nome()].append((node2.get_nome(), weight))
+        if not self.oriented and (node1.get_nome(),weight) not in self.grafo[node2.get_nome()] and not self.oriented:
+            self.grafo[node2.get_nome()].append((node1.get_nome(), weight))
+
+
+
+
+    
+
--- main.py
+++ main.py
@@ -6,40 +6,47 @@ from Player import Player
 def main():
     y = input("Digita o comprimento do mapa:")
     x = input("Digita a largura do mapa:")
-    metax = input("Digite a posição x da meta:")
-    metay = input("Digite a posição y da meta:")
-    estadox = input("Digite a posição x do carro:")
-    estadoy = input("Digite a posição y do carro:")
-    i=0
-    j=0
+    metax = int(x)/2
+    metay = int(y)
+    estadox = input("Digite a posição x inicial:")
+    estadoy = input("Digite a posição y inicial:")
+    nobs=3
+    i=1
+    j=1
     lineMap=""
     mapLines=list()
-    pecas=['X','-']
-    while(j<int(x)):
-        while(i<int(y)):
-            if(j==0):
+    while(j<=int(x)):
+        while(i<=int(y)):
+            if(j==1):
                 lineMap+="X"
+            elif(nobs<3 and i==(int(y)//2)):
+                    lineMap+="X"
+                    nobs+=1
             elif(j==int(estadox) and i==int(estadoy)):
                 lineMap+="P"
-            elif(j==int(metax) and i==int(metay)):
-                lineMap+="F"
             elif(j==int(metax)-1 and i==int(metay)):
+                lineMap+="F"
+            elif(j==int(metax) and i==int(metay)):
                 lineMap+="F"
             elif(j==int(metax)+1 and i==int(metay)):
                 lineMap+="F"
-            elif(i==0):
+            elif(i==1):
                 lineMap+="X"
-            elif(i==int(y)-1):
+            elif(i==int(y)):
                 lineMap+="X"    
-            elif(j==int(x)-1):
+            elif(j==int(x)):
                 lineMap+="X"
             else:
-                lineMap+=pecas[random.randint(0,1)]
+                lineMap+="-"
             i+=1
+        if(j==(int(x)//2)):
+            nobs=0
+        if(j==(int(x)//4)):
+            nobs=0
         mapLines.append(lineMap)
         lineMap=""
         j+=1
-        i=0
+        i=1
 
     with open('map.txt', 'w') as filename:
         for line in mapLines:
@@ -54,13 +61,13 @@ def main():
     for linha in l:
         for x in linha:
             if(x=='X'):
-                p = Peca("wall_"+str(countLine)+"_"+str(countColumn),countLine,countColumn,"WALL")
+                p = Peca("parede("+str(countLine)+","+str(countColumn)+")",countLine,countColumn,"WALL")
             if(x=='-'):
-                p = Peca("none_"+str(countLine)+"_"+str(countColumn),countLine,countColumn,"NONE")
+                p = Peca("vazio("+str(countLine)+","+str(countColumn)+")",countLine,countColumn,"NONE")
             if(x=='F'):
-                p = Peca("final_"+str(countLine)+"_"+str(countColumn),countLine,countColumn,"FINAL")        
+                p = Peca("meta("+str(countLine)+","+str(countColumn)+")",countLine,countColumn,"FINAL")        
             if(x=='P'):
-                p = Peca("none_"+str(countLine)+"_"+str(countColumn),countLine,countColumn,"None")
+                p = Peca("vazio("+str(countLine)+","+str(countColumn)+")",countLine,countColumn,"None")
                 player = Player("player1",countLine,countColumn,0,0,0,0)
             allPieces.append(p)
             countColumn +=1
@@ -68,6 +75,17 @@ def main():
         countColumn=1
     #for p in allPieces:
     #    print(p.get_nome())
+    g = Grafo(allPieces)
+    for piece in allPieces:
+        g.constroiGrafo(g.devolvePecabyNome(piece))
+
+    for key in g.grafo.keys():
+        print(str(key)+str("->"),end=' ')
+        for (n,custo) in g.grafo[key]:
+             print((n,custo),end=' ')
+        print("\n")
+
+
 
 if __name__ == "__main__":
     main()
